dfd46dab4fd6f084c03cc8b08fa122037ad73e9c
Nahu <nahuel@nahuelwexd.com>
2023-01-26 10:03:17 -0300

    add docs for IcuFieldPositionIterator

--- src/icu-field-position-iterator.c
+++ src/icu-field-position-iterator.c
@@ -29,6 +29,15 @@ icu_field_position_iterator_free (IcuFieldPositionIterator *self)
   g_slice_free (IcuFieldPositionIterator, self);
 }
 
+/**
+ * icu_field_position_iterator_new:
+ * @error: (out) (optional): The return location for a recoverable
+ *   error.
+ *
+ * Creates a new [class@FieldPositionIterator].
+ *
+ * Returns: A newly created [class@FieldPositionIterator].
+ */
 IcuFieldPositionIterator *
 icu_field_position_iterator_new (GError **error)
 {
@@ -45,6 +54,15 @@ icu_field_position_iterator_new (GError **error)
   return g_steal_pointer (&self);
 }
 
+/**
+ * icu_field_position_iterator_ref:
+ * @self: A [class@FieldPositionIterator].
+ *
+ * Increases the reference count of `self` by one.
+ *
+ * Returns: (transfer full): The passed in
+ *   [class@FieldPositionIterator].
+ */
 IcuFieldPositionIterator *
 icu_field_position_iterator_ref (IcuFieldPositionIterator *self)
 {
@@ -56,6 +74,15 @@ icu_field_position_iterator_ref (IcuFieldPositionIterator *self)
   return self;
 }
 
+/**
+ * icu_field_position_iterator_unref:
+ * @self: A [class@FieldPositionIterator].
+ *
+ * Decreases the reference count of `self` by one.
+ *
+ * If the resulting reference count is zero, frees the memory of
+ * `self`.
+ */
 void
 icu_field_position_iterator_unref (IcuFieldPositionIterator *self)
 {
@@ -68,8 +95,22 @@ icu_field_position_iterator_unref (IcuFieldPositionIterator *self)
 
 /**
  * icu_field_position_iterator_next:
- * @begin_index: (out) (optional):
- * @end_index: (out) (optional):
+ * @self: A [class@FieldPositionIterator].
+ * @begin_index: (out) (optional): Set to the start offset of the
+ *   field in the formatted string (undefined if the method returns
+ *   a negative value). Ignored if `NULL`.
+ * @end_index: (out) (optional): Set to the end offset of the field
+ *   in the formatted string (undefined if the method returns a
+ *   negative value). Ignored if `NULL`.
+ *
+ * Get information for the next field in the formatted string to
+ * which `self` currently applies, or return a negative value if
+ * there are no more fields.
+ *
+ * Returns: The field type (non-negative value), or a negative value
+ *   if there are no more fields for which to provide information.
+ *   If negative, then any values pointed by `begin_index` and
+ *   `end_index` are undefined.
  */
 gint32
 icu_field_position_iterator_next (IcuFieldPositionIterator *self,
